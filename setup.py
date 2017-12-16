4112443150d8e7b495ce5df55fadab211af6a600
Evyatar Meged <cambina1@gmail.com>
2017-12-16 15:50:41 +0200

    pytrend-cli now using click library to parse CLI args

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ setup(
     name='pytrend-cli',
     packages=['pytrend_cli'],
     license='MIT',
-    version='1.14',
+    version='1.16',
     description='Get trending GitHub repositories daily/weekly/monthly and by language',
     author='Evyatar Meged',
     author_email='evyatarmeged@gmail.com',
     url='https://github.com/evyatarmeged/pytrend-cli',
     keywords=['GitHub', 'trending', 'repositories', 'developers', 'scraping'],
-    classifiers=['Programming Language :: Python :: 3'],
     install_requires=['beautifulsoup4', 'requests', 'lxml'],
     entry_points={
         'console_scripts': [
-            'pytrend = pytrend_cli.pytrend:entry_point'
+            'pytrend = pytrend_cli.pytrend:main'
         ]
     },
 )
